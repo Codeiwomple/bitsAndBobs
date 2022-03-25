59218f11769d1ddade8c1e8d86ccf39ae8870205
WireInTheGhost <63163946+WireInTheGhost@users.noreply.github.com>
2022-03-25 14:34:56 +0000

    Update and rename AES_forReview.cpp to simpleAES.cpp

--- simpleAES.cpp
+++ simpleAES.cpp
@@ -1,3 +1,14 @@
+/*
+This program impliments the 128bit varient of the Rijndael Cipher in C++    
+As per FIP197. Output for each iteration has been checked against FIPS197
+example outputs and shown to be working correctly. 
+
+Currently it is only set up to encrypt a single block, but functions for 
+different modes of operation could easily be added.
+
+Not optimised or intened for high performance. Simply a learning exercise. 
+*/
+
 #include <iostream>
 #include <array>
 
